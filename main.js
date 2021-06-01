ea3235ce57089a2d6f243d7a50e4313ba0269b31
tran nhat <tranminhnhat1005@gmail.com>
2021-06-01 20:17:50 +0700

    master - create cocos2d-x project

---
+++ main.js
@@ -0,0 +1,14 @@
+cc.game.onStart = function () {
+	console.log(cc);
+	cc.view.setDesignResolutionSize(800, 450, cc.ResolutionPolicy.SHOW_ALL);
+	cc.view.resizeWithBrowserSize(true);
+	//load resources
+	cc.LoaderScene.preload(
+		g_resources,
+		function () {
+			cc.director.runScene(new GameScene()); //run the GameScene
+		},
+		this
+	);
+};
+cc.game.run();
